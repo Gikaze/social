554b0567e91baca155c6a61a148aeb29a9cbe3b4
Gikaze <gillesromy@outlook.de>
2021-07-28 09:30:53 +0200

    post routes finished/ REST API completed

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { findByIdAndDelete } = require("../models/Post");
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 // create a post
 router.post("/", async(req, res) => {
@@ -60,9 +61,40 @@ router.delete("/:id", async(req, res) => {
     }
 }) 
 
-// like a post
-// unlike a post
+// like or dislike a post
 
+router.put("/:id/like", async(req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push:{likes:req.body.userId}});
+            res.status(200).json(`The post has been liked`)
+        }else{
+            await post.updateOne({$pull:{likes:req.body.userId}});
+            res.status(200).json(`The post has been disliked`)
+        }
 
+    }catch(err){
+        res.status(500).json(err)
+    }
+}) 
+
+// timeline post
+
+router.get("/timeline/all", async(req, res) =>{
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId:currentUser._id});
+        const friendPost = await Promise.all(
+        currentUser.followings.map(friendId =>{
+            return Post.find({ userId: friendId})
+            })
+        );
+        res.status(200).json(userPosts.concat(...friendPost));
+        
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
